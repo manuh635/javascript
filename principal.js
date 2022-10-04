3ab1be3f9717ad211dc900908d6211975ba16f7f
manuh635 <105725862+manuh635@users.noreply.github.com>
2022-10-04 09:18:47 -0300

    Create principal.js

---
+++ principal.js
@@ -0,0 +1,16 @@
+introducao-javascript
+├── css
+│   ├── index.css
+│   └── reset.css
+├── js
+│   └── principal.js
+├── img
+├── favicon.ico
+└── index.html
+// Restante do código HTML
+            </section>
+        </main>
+        <!-- Importação dos Javascripts AQUI -->
+        <script src="js/principal.js"></script>
+    </body>
+</html>
